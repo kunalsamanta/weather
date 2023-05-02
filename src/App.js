b64ee749214bb42aae650deb44dff375cb9ca741
Kunal Samanta <kunalsamanta5@gmail.com>
2023-05-02 11:05:43 +0530

    style the code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import React from 'react';
+import Tempapp from './components/template';
 
 function App() {
   return (
     <div className="App">
-    
+      <Tempapp/>
+
     </div>
   );
 }
---
+++ src/components/template.js
@@ -0,0 +1,19 @@
+import  React from 'react';
+import "./style.css";
+
+
+const Tempapp=()=>{
+    return(
+        <>
+            <div className="box">
+                <inpit
+                type="search"
+                className="input-box" 
+                ></inpit>
+            </div>
+        </>
+
+
+    )
+}
+export default Tempapp;
